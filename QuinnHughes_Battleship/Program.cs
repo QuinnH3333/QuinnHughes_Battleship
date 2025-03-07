bcf34d0626e8568d6f344d021fc5b5e42f4a4ab8
CHughes <Catherinehughes0317@gmail.com>
2025-03-06 18:50:23 -0600

    Add project files.

---
+++ QuinnHughes_Battleship/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace QuinnHughes_Battleship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
